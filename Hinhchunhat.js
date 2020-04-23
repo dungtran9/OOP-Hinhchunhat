853b4344204e9cd0de46699c0b6fd76ce8afd2e6
dungtran9 <63217229+dungtran9@users.noreply.github.com>
2020-04-23 10:31:46 +0700

    o

--- Hinhchunhat.js
+++ Hinhchunhat.js
@@ -13,25 +13,25 @@ function getRandomHex(){
     return Math.floor(Math.random()*255);
 }
 function getRandomColor(){
-    var red = getRandomHex();
-    var green = getRandomHex();
-    var blue = getRandomHex();
+    let red = getRandomHex();
+    let green = getRandomHex();
+    let blue = getRandomHex();
     return "rgb(" + red + "," + blue + "," + green +")";
 }
 function createRectangle() {
-    var canvas =document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    var color = getRandomColor();
-    var height = Math.floor(Math.random() * innerHeight);
-    var width = Math.floor(Math.random() * innerWidth);
-    var x = Math.floor(Math.random() * innerWidth);
-    var y = Math.floor(Math.random() * innerHeight);
-    var rectangle = new Rectangle(height,width);
+    let canvas =document.getElementById("myCanvas");
+    let ctx = canvas.getContext("2d");
+    let color = getRandomColor();
+    let height = Math.floor(Math.random() * innerHeight);
+    let width = Math.floor(Math.random() * innerWidth);
+    let x = Math.floor(Math.random() * innerWidth);
+    let y = Math.floor(Math.random() * innerHeight);
+    let rectangle = new Rectangle(height,width);
     ctx.fillStyle =color;
     ctx.fillRect(x,y,rectangle.width,rectangle.height);
-    var area = rectangle.getArea();
+    let area = rectangle.getArea();
     console.log("dien tich : " +area);
-    var chuvi =rectangle.getChuVi();
+    let chuvi =rectangle.getChuVi();
     console.log("chu vi : "+chuvi);
 
 }
